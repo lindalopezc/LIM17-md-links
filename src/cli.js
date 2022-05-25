5b0a40d1a323b3de8ff7916997a7300a1a84a465
lindalopezc <lindalopezramos61@gmail.com>
2022-05-24 20:13:36 -0500

    modified eslint

--- src/cli.js
+++ src/cli.js
@@ -1,43 +1,36 @@
 #!/usr/bin/env node
+/* eslint-disable no-console */
 
-const {mdLinks} = require('./md-links.js');
+const { mdLinks } = require('./md-links');
 
 const inputsArray = process.argv;
 const inputPath = inputsArray[2];
 const options = [inputsArray[3], inputsArray[4]];
 
-if (inputPath){
-    if(inputsArray.length === 3){
-        mdLinks(inputPath, {validate: false, stats: false})
-        .then(resolve => console.log(resolve))
-        .catch(error => console.log(error));
+if (inputPath) {
+  if (inputsArray.length === 3) {
+    mdLinks(inputPath, { validate: false, stats: false })
+      .then((resolve) => console.log(resolve))
+      .catch((error) => console.log(error));
+  } else if (inputsArray.length === 4) {
+    if (options.includes('--validate')) {
+      mdLinks(inputPath, { validate: true, stats: false })
+        .then((resolve) => console.log(resolve))
+        .catch((error) => console.log(error));
+    } else if (options.includes('--state')) {
+      mdLinks(inputPath, { validate: false, stats: true })
+        .then((resolve) => console.log(resolve))
+        .catch((error) => console.log(error));
+    } else {
+      console.log('La opción ingresada no es válida. Opciones permitidas: --validate o --stats');
     }
-    else if(inputsArray.length === 4){
-        if(options.includes('--validate')){
-            mdLinks(inputPath, {validate: true, stats: false})
-            .then(resolve => console.log(resolve))
-            .catch(error => console.log(error));
-        }
-        else if(options.includes('--state')){
-            mdLinks(inputPath, {validate: false, stats: true})
-            .then(resolve => console.log(resolve))
-            .catch(error => console.log(error));
-        }
-        else{
-            console.log('La opción ingresada no es válida. Opciones permitidas: --validate o --stats')
-        }
-    }
-    else{
-        if(options.includes('--validate') && options.includes('--stats')){
-            mdLinks(inputPath, {validate: true, stats: true})
-            .then(resolve => console.log(resolve))
-            .catch(error => console.log(error));
-        }
-        else{
-            console.log('Hay opciones ingresadas no válidas. Opciones permitidas: --validate o --stats')
-        }
-    }
-}
-else{
-    console.log('Error: Debe ingresar una ruta. Vuelva a intentarlo')
+  } else if (options.includes('--validate') && options.includes('--stats')) {
+    mdLinks(inputPath, { validate: true, stats: true })
+      .then((resolve) => console.log(resolve))
+      .catch((error) => console.log(error));
+  } else {
+    console.log('Hay opciones ingresadas no válidas. Opciones permitidas: --validate o --stats');
+  }
+} else {
+  console.log('Error: Debe ingresar una ruta. Vuelva a intentarlo');
 }
--- src/md-links.js
+++ src/md-links.js
@@ -1,75 +1,63 @@
+/* eslint-disable max-len */
 const {
   checkPathExists,
-  checkPathIsDirectory, 
-  getExtension, 
-  readDirectory, 
-  saveFiles, 
+  checkPathIsDirectory,
+  getExtension,
+  readDirectory,
+  saveFiles,
   filterMdFiles,
   getLinks,
-  getStatusLink
-} = require('./index.js');
+  getStatusLink,
+} = require('./index');
 
-const mdLinks = (path, options = {validate: false, stats: false}) => {
-  return new Promise((resolve, reject) => {
-    if(checkPathExists(path)){
-      let linksArray;
-      if(checkPathIsDirectory(path)){
-        const directoryContent = readDirectory(path);
-        if(directoryContent){
-          const filesArray = saveFiles(directoryContent);
-          const mdFilesArray = filterMdFiles(filesArray);
-          if(mdFilesArray.length>0){
-            linksArray = getLinks(mdFilesArray);
-            
-            if(options.validate && options.stats){ //validate: true, stats: true
-              getStatusLink(linksArray)
-              .then(response => {
-                const uniqueLinks = [...new Set(response.map(element => element.href))]; 
-                const brokensLinks = response.filter((element)=> element.ok === 'fail');
-               resolve(`Total: ${response.length} \nUnique: ${uniqueLinks.length} \nBroken: ${brokensLinks.length}`);
+const mdLinks = (path, options = { validate: false, stats: false }) => new Promise((resolve, reject) => {
+  if (checkPathExists(path)) {
+    let linksArray;
+    if (checkPathIsDirectory(path)) {
+      const directoryContent = readDirectory(path);
+      if (directoryContent) {
+        const filesArray = saveFiles(directoryContent);
+        const mdFilesArray = filterMdFiles(filesArray);
+        if (mdFilesArray.length > 0) {
+          linksArray = getLinks(mdFilesArray);
+
+          if (options.validate && options.stats) { // validate: true, stats: true
+            getStatusLink(linksArray)
+              .then((response) => {
+                const uniqueLinks = [...new Set(response.map((element) => element.href))];
+                const brokensLinks = response.filter((element) => element.ok === 'fail');
+                resolve(`Total: ${response.length} \nUnique: ${uniqueLinks.length} \nBroken: ${brokensLinks.length}`);
               });
-            }
-            else if(options.validate && !options.stats){ //validate: true, stats: false
-              getStatusLink(linksArray)
-              .then(response => resolve(response));
-            }
-            else if(!options.validate && options.stats){ //validate: false, stats: true
-              getStatusLink(linksArray)
-              .then(response => {
-                const uniqueLinks = [...new Set(response.map(element => element.href))]; 
-               resolve(`Total: ${response.length} \n Unique: ${uniqueLinks.length}`);
+          } else if (options.validate && !options.stats) { // validate: true, stats: false
+            getStatusLink(linksArray)
+              .then((response) => resolve(response));
+          } else if (!options.validate && options.stats) { // validate: false, stats: true
+            getStatusLink(linksArray)
+              .then((response) => {
+                const uniqueLinks = [...new Set(response.map((element) => element.href))];
+                resolve(`Total: ${response.length} \n Unique: ${uniqueLinks.length}`);
               });
-            }
-            else{ //Validate: false, stats: false
-              resolve(linksArray);
-            }
+          } else { // Validate: false, stats: false
+            resolve(linksArray);
           }
-          else{
-            reject('No se encontró ningun archivo Markdown');
-          }
+        } else {
+          reject(new Error('No se encontró ningun archivo Markdown'));
         }
-        else {
-          reject('Este directorio está vacío');
-        }
-      } 
-      else{
-        if(getExtension(path)==='.md'){
-          linksArray = getLinks([path]);
-          if(options.validate){ //validate:true, stats:false
-            getStatusLink(linksArray).then(response => resolve(response));
-          }
-          else{ //validate:false
-            resolve(linksArray);//Array de objetos
-          }
-        }
-        else{
-          reject('La ruta ingresada no contiene un archivo markdown(.md)');
-        }
+      } else {
+        reject(new Error('Este directorio está vacío'));
       }
+    } else if (getExtension(path) === '.md') {
+      linksArray = getLinks([path]);
+      if (options.validate) { // validate:true, stats:false
+        getStatusLink(linksArray).then((response) => resolve(response));
+      } else { // validate:false
+        resolve(linksArray);// Array de objetos
+      }
+    } else {
+      reject(new Error('La ruta ingresada no contiene un archivo markdown(.md)'));
     }
-    else{
-      reject('La ruta ingresada no existe. Ingrese nuevamente una ruta.');
-    }
-  })
-}
-  module.exports = {mdLinks}; 
+  } else {
+    reject(new Error('La ruta ingresada no existe. Ingrese nuevamente una ruta.'));
+  }
+});
+module.exports = { mdLinks };
